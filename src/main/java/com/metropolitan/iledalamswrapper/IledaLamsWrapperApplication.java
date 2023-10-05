2a84da8b2030550682be258da9232a0c0c836f51
Jovana Jovic <jovana.jovic@metropolitan.ac.rs>
2023-10-05 16:55:45 +0200

    Initial commit

---
+++ src/main/java/com/metropolitan/iledalamswrapper/IledaLamsWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.iledalamswrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IledaLamsWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IledaLamsWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/iledalamswrapper/IledaLamsWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.iledalamswrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IledaLamsWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
